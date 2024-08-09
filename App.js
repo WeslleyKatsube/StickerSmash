15736146615f2826c2ff7f4e9c04c4a5529357b2
WeslleyKatsube <161661648+WeslleyKatsube@users.noreply.github.com>
2024-08-09 20:05:28 -0300

    a

--- App.js
+++ App.js
@@ -8,7 +8,20 @@ import { useState } from 'react';
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [showAppOptions, setShowAppOptions] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    // we will implement this later
+  };
+
+  const onSaveImageAsync = async () => {
+    // we will implement this later
+  };
+
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -18,6 +31,7 @@ export default function App() {
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       alert('You did not select any image.');
     }
@@ -30,10 +44,21 @@ export default function App() {
           selectedImage={selectedImage}
         />
     </View>
+              {showAppOptions ? (
+              <style ={styles.optionsContainer}>
+              <View style={styles.optionsContainer}>
+              <View style={styles.optionsRow}>
+                <IconButton icon="refresh" label="Reset" onPress={onReset} />
+                <CircleButton onPress={onAddSticker} />
+                <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+              </View>
+            </View>
+            ) : (
     <View style={styles.footerContainer}>
         <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-        <Button label="Use this photo" />
+        <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
       </View>
+      )}
     <StatusBar style="auto" />
   </View>
   );
@@ -59,6 +84,16 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
+  },
+
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 
 });
